4763387f2dc79c08f64bf8dfae12dc8497107aeb
Prashant Garbuja <prashantmaha@yahoo.com>
2022-05-01 01:05:13 +1000

    Lifting the State Up

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,20 +1,12 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  state = {
-    value: this.props.counter.value,
-  };
-
-  handleIncrement = () => {
-    this.setState({ value: this.state.value + 1 });
-  };
-
   render() {
     return (
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={this.handleIncrement}
+          onClick={() => this.props.onIncrement(this.props.counter)}
           className="btn btn-secondary btn-sm"
         >
           Increment
@@ -30,13 +22,13 @@ class Counter extends Component {
   }
 
   getBadgeClasses() {
-    let classes = "badge m-2 alert-";
-    classes += this.state.value === 0 ? "warning" : "primary";
+    let classes = "badge m-2 bg-";
+    classes += this.props.counter.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { value } = this.state;
+    const { value } = this.props.counter;
     return value === 0 ? "Zero" : value;
   }
 }
